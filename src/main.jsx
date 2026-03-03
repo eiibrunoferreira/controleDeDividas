6e7b8ebe1f6869aeb7e5bd25c9c6a5cec23e29a3
Bruno Ferreira <brunoferreirabf2002@gmail.com>
2026-03-03 13:02:12 -0300

    envio de arquivos e updates

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </DebtProvider>
   </React.StrictMode>
 );
+
+// Remove splash screen quando tudo carregar
+/*window.addEventListener("load", () => {
+  const splash = document.getElementById("splash-screen");
+  if (splash) {
+    splash.classList.add("fade-out");
+    setTimeout(() => splash.remove(), 500);
+  }
+});*/
