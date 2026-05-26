9e000acae57a8afccc712cea0cf7c5bba88d5a93
Bruno Ferreira <brunoferreirabf2002@gmail.com>
2026-05-26 00:10:40 -0300

    resolvendo bug navHeader

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -4,7 +4,7 @@ export default function NavHeader() {
   const navigate = useNavigate();
 
   return (
-    <div onClick={() => navigate(-1)} className="fixed ml-2 cursor-pointer -mt-1.5 w-11 h-11 flex items-center justify-center bg-[#10284D] rounded-full hover:brightness-90 transition-all">
+    <div onClick={() => navigate(-1)} className="absolute ml-2 cursor-pointer -mt-1.5 w-11 h-11 flex items-center justify-center bg-[#10284D] rounded-full hover:brightness-90 transition-all">
       <button
         className="text-white text-xl font-bold transition"
       >
